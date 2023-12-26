67395bf319650284aa6f6e216e5da99f2a0a9dfd
Matias <matiasescobar@yahoo.com.ar>
2023-12-26 15:48:11 -0300

    Se modifico Item Detail para agregar la libreria React Slick y utilizar el carousel

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ This template provides a minimal setup to get React working in Vite with HMR and
 
 Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-# PrimerPreEntrega-EscobarMatias
+-   [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+-   [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# SegundaPreEntrega-EscobarMatias
+
+Use librerias:
+
+-   Tailwind (https://tailwindcss.com/),
+-   NextUi (https://nextui.org/),
+-   Toastify (https://apvarun.github.io/toastify-js/),
+-   React Icons (https://react-icons.github.io/react-icons/),
+-   React Slick (https://react-slick.neostack.com/),
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,10 +1,13 @@
 import { Spinner, Button } from "@nextui-org/react";
 import { useEffect, useState } from "react";
-import "./ItemDetail.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import ItemCount from "../ItemCount/ItemCount";
 import { ToastContainer, Zoom, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
+import "./ItemDetail.css";
 
 const ItemDetail = ({ producto }) => {
     const [cargando, setCargando] = useState(true);
@@ -20,6 +23,7 @@ const ItemDetail = ({ producto }) => {
             transition: Zoom,
         });
     };
+
     useEffect(() => {
         // Simulación de una carga asíncrona
         const cargarDatos = async () => {
@@ -29,6 +33,15 @@ const ItemDetail = ({ producto }) => {
         cargarDatos();
     }, []);
 
+    // Configuración del carousel
+    const carouselSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
     return (
         <div>
             {cargando ? (
@@ -36,29 +49,51 @@ const ItemDetail = ({ producto }) => {
                     <Spinner label="Cargando" color="warning" labelColor="warning" />
                 </div>
             ) : (
-                <div className="item-detail">
-                    <img className="imagen" src={producto.imagen1} alt={producto.nombre} />
-                    <div className="texto">
-                        <h2 className="detalle-nombre">{producto.nombre} </h2>
-                        <p className="detalle-descripcion">{producto.descripcion}</p>
-                        <p className="detalle-precio">${producto.precio}</p>
-
-                        {toggle ? (
-                            <div className="boton space-x-6">
-                                <Link to="/carrito">
-                                    <Button color="warning" className="mt-4">
-                                        Terminar mi compra
-                                    </Button>
-                                </Link>
-                                <Link to="/">
-                                    <Button color="warning" className="mt-4">
-                                        Seguir comprando
-                                    </Button>
-                                </Link>
+                <div className="contenedor-general">
+                    <div className="carousel">
+                        <Slider {...carouselSettings}>
+                            <div>
+                                <img
+                                    className="imagen"
+                                    src={producto.imagen1}
+                                    alt={producto.nombre}
+                                />
                             </div>
-                        ) : (
-                            <ItemCount stock={producto.stock} agregarAlCarrito={agregarAlCarrito} />
-                        )}
+                            <div>
+                                <img
+                                    className="imagen"
+                                    src={producto.imagen2}
+                                    alt={producto.nombre}
+                                />
+                            </div>
+                        </Slider>
+                    </div>
+                    <div className="item-detail">
+                        <div className="texto">
+                            <h2 className="detalle-nombre">{producto.nombre} </h2>
+                            <p className="detalle-descripcion">{producto.descripcion}</p>
+                            <p className="detalle-precio">${producto.precio}</p>
+
+                            {toggle ? (
+                                <div className="boton space-x-6">
+                                    <Link to="/carrito">
+                                        <Button color="warning" className="mt-4">
+                                            Terminar mi compra
+                                        </Button>
+                                    </Link>
+                                    <Link to="/">
+                                        <Button color="warning" className="mt-4">
+                                            Seguir comprando
+                                        </Button>
+                                    </Link>
+                                </div>
+                            ) : (
+                                <ItemCount
+                                    stock={producto.stock}
+                                    agregarAlCarrito={agregarAlCarrito}
+                                />
+                            )}
+                        </div>
                     </div>
                 </div>
             )}
